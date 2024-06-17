40236022c1290bc0aad10db5c4f87d36b5d97142
fcopbz <francisco.perez@crealogix.com>
2024-06-17 15:29:26 +0200

    added tailwind and basic structure

---
+++ src/modules/pokemon/pages/PokemonGame.vue
@@ -0,0 +1,19 @@
+<template>
+  <section v-if="false" class="flex flex-col justify-center items-center w-screen h-screen">
+    <h1 class="text-3xl animate-pulse">Loading...</h1>
+  </section>
+
+  <section class="flex flex-col justify-center items-center w-screen h-screen">
+    <h1 class="text-3xl m-5">Who's that Pokemon</h1>
+    <!-- Pokemon picture -->
+    <PokemonPicture />
+
+    <!-- Pokemon options -->
+    <PokemonOptions />
+  </section>
+</template>
+
+<script setup lang="ts">
+import PokemonOptions from '../components/PokemonOptions.vue';
+import PokemonPicture from '../components/PokemonPicture.vue';
+</script>
